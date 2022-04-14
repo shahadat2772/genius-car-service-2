576c16603a9973c3291366043b8577ea37c98310
shahadat2772 <shahadat2772@gmailcom>
2022-04-14 17:08:38 +0600

    toast is now working

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,31 +1,70 @@
+import { async } from "@firebase/util";
 import React, { useEffect, useRef } from "react";
-import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import toast, { Toaster } from "react-hot-toast";
+import {
+  useSendPasswordResetEmail,
+  useSignInWithEmailAndPassword,
+} from "react-firebase-hooks/auth";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { auth } from "../../../firebase.init";
+import Loading from "../../Shared/Loading/Loading";
 import SocialLogin from "../SocialLogin/SocialLogin";
 import "./Login.css";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   const [signInWithEmailAndPassword, user, loading, error] =
     useSignInWithEmailAndPassword(auth);
 
+  const [sendPasswordResetEmail, sending, passeordResetError] =
+    useSendPasswordResetEmail(auth);
+
+  const from = location?.state?.from?.pathname || "/home";
+
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+
   const resetPassword = () => {
-    console.log("Hello reset");
+    const email = emailRef.current.value;
+    if (email) {
+      sendPasswordResetEmail(email);
+      toast("Reset email sended.");
+    } else {
+      toast("Please enter your email.");
+    }
   };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    await signInWithEmailAndPassword(email, password);
+  };
+  if (user) {
+    navigate(from, { replace: true });
+  }
 
   return (
     <div className="container">
       <div className="formContainer">
         <h3 className="text-center text-secondary pb-2">Login</h3>
-        <form action="">
+        <form onSubmit={handleSubmit} action="">
           <div className="input-fields">
             <label htmlFor="email">Enter email</label>
-            <input type="email" name="email" id="email" />
+            <input ref={emailRef} type="email" name="email" id="email" />
           </div>
           <div className="input-fields">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
+            <input
+              ref={passwordRef}
+              type="password"
+              name="password"
+              id="password"
+            />
           </div>
+          <span className="text-danger">{error?.message}</span>
           <input className="submitBtn" type="submit" value="Login" />
           <p className="text-center mt-1 mb-1">
             <small>
@@ -48,6 +87,7 @@ const Login = () => {
             </small>
           </p>
         </form>
+        <Toaster />
         <SocialLogin></SocialLogin>
       </div>
     </div>
